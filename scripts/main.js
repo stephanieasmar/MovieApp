41b9942395bdbdbfdce403dad9bf4b34aad8c44e
Stephanie Asmar <sasmar8@gmail.com>
2017-07-25 15:15:37 -0400

    Added function that draws movie data to empty HTML elements, changed getMovieData callback to promise

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,59 @@
-// GOALS: Use promises to run functions that draws the movie information/poster to the DOM
+var movieURL="http://www.omdbapi.com";
+var posterURL="http://img.omdbapi.com";
 
+var MOVIE_SEARCH_SELECTOR = '[data-movie-search="movie-search"]'
+var FORM_SELECTOR = '[data-form-role="form"]'
+var MOVIE_SECTION_SELECTOR = '[data-movie-info="movie-info"]'
+var MOVIE_TITLE_DISPLAY = '[data-title-display="target"]'
+var POSTER_IMAGE_DISPLAY = '[data-poster-display="target"]'
 
-//STEPS:
-//saveMovie: saves the movie data to local storage
-//removeMovie: removes saved movie data from local storage
-//listMovies: lists all movies curently located in local storage
-
-var movieURL="http://www.omdbapi.com/?i=tt3896198&apikey=1ca32dee";
-var posterURL="http://img.omdbapi.com/?i=tt3896198&h=600&apikey=1ca32dee";
-
-MOVIE_TITLE_SELECTOR = '[data-movie-search="movie-title"]'
-SEARCH_BUTTON_SELECTOR = '[data-search-role="search-button"]'
-
-function makeMovieElement() {
-    var movieElement = document.createElement('div');
-    document.body.appendChild(movieElement);
-}
-
-function makePosterElement() {
-    var posterElement = document.createElement('div');
-    document.body.appendChild(posterElement);
-}
-
-function getUserInput(textArea) {
-    var $input = $(textArea); 
-    var $key = $input.attr('name');
-    var $value = $input.val(); 
-
-
-function getMovieData() {
-    $.get(movieURL, function(movieData) {
-        console.log(movieData);
-    });
-}
-
-function getPosterData() {
-    $.get(posterURL, function(posterData) {
-        console.log(posterData);
-    });
-}
-
-
-
-
-//************************STUFF**************************//
-
-
-// function main() {
-//     getMovieData()
-//         .then(doSomthingHere);
+// function makeMovieElement() {
+//     var movieElement = document.createElement('div');
+//     document.body.appendChild(movieElement);
 // }
+
+// function makePosterElement() {
+//     var posterElement = document.createElement('div');
+//     document.body.appendChild(posterElement);
+// }
+
+function getMovieData(searchTerms) {
+    return $.get(movieURL, searchTerms)
+}
+
+function drawMovieTitle(data) {
+    for(i=0; i<2; i++){
+    title = data["Search"][i]["Title"]
+    year = data["Search"][i]["Year"]
+    $(MOVIE_TITLE_DISPLAY)[i].textContent = (title + ", " + year)
+    }
+}
+
+function getPosterData(searchTerms) {
+    return $.get(posterURL, searchTerms, function(data) {
+        console.log(data)
+    });
+}
+
+// function drawPosterData(data) {
+//     for(i=0; i<2; i++){
+//     poster = data["Search"][i]["Title"]
+//     $(POSTER_IMAGE_DISPLAY)[i] = (poster)
+//     }
+// }
+
+$(FORM_SELECTOR).on("submit", function(event) {
+    event.preventDefault();
+    var data = {
+    "s": " ",
+    "apikey": "1ca32dee"
+    }
+    data.s = $(MOVIE_SEARCH_SELECTOR).val();
+    getMovieData(data)
+        .then(drawMovieTitle)
+});
+
+
+function main() {
+
+}
